13325bf0cc43716fdaa1e351989fa407a651cfe6
34j <34j.github@proton.me>
2025-12-25 13:09:17 +0900

    feat: migrate to proto datastore

--- app/src/main/java/com/mikoz/sms2email/ui/theme/Theme.kt
+++ app/src/main/java/com/mikoz/sms2email/ui/theme/Theme.kt
@@ -1,25 +1,35 @@
 package com.mikoz.sms2email.ui.theme
 
+import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 
 // Simplified theme with basic light/dark switching using Material3 defaults.
 @Composable
 fun SMS2EmailTheme(content: @Composable () -> Unit) {
   val darkTheme = isSystemInDarkTheme()
   val colorScheme =
-      if (darkTheme)
-          darkColorScheme(
-              primary = Color(0xEEFFEDA6),
-              secondary = Color(0xFF976e4f),
-              tertiary = Color(0xFFFFFF6D),
-              background = Color(0xFF00000a),
-          )
-      else lightColorScheme()
+      when {
+        !darkTheme && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+          val context = LocalContext.current
+          if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
+        darkTheme ->
+            darkColorScheme(
+                primary = Color(0xEEFFEDA6),
+                secondary = Color(0xFF976e4f),
+                tertiary = Color(0xFFFFFF6D),
+                background = Color(0xFF00000a),
+            )
+        else -> lightColorScheme()
+      }
   MaterialTheme(
       colorScheme = colorScheme,
       content = content,
